6b17f877f237c34cd1bdeb52943821ea2b45e631
Brijendra Sial <brijendrasial1987@gmail.com>
2014-12-26 22:31:42 +0530

    Steamtrades.com Automation
    Steamtrades.com Automation

---
+++ st_settings.py
@@ -0,0 +1,48 @@
+
+def write_link():
+    file_name = 'links_steam_trades.txt'
+    print '\nEnter your thread url\n'
+    url = raw_input()
+    if url is '':
+        print '\nNo Url entered\n'
+        write_link()
+    else:
+        f = open('links_steam_trades.txt', 'a')
+        f.write(url + '\n')
+        f.close
+        print'\nURL successfully saved in %s\n'%(file_name)
+
+def write_cookies():
+    print '\nEnter the new cookie name for steamtrades.com\n'
+    cookie_name = raw_input()
+    f = open('cookie_steam_trades.txt', 'w')
+    f.write(cookie_name + '\n')
+    f.close
+    f = open('cookie_steam_trades.txt', 'a')
+    print '\nEnter the value for %s'%cookie_name
+    cookie_value = raw_input()
+    f.write(cookie_value + '\n')
+    f.close
+    
+
+print '####################################################################'
+print '\nThis script is for setting the configuration adding url & cookies\n'
+message = "1.Enter your steamtrades.com thread url\n2.Enter your steamtrades.com cookie\n3.Exit\n\n"
+while  True:
+    print message
+    try:
+        no = raw_input()
+        if no is '1':
+            write_link() 
+        elif no is '2':
+            write_cookies()
+
+        elif no is '3':
+            exit()
+        else:
+            print'\n Wrong input \n'
+
+    except NameError, e:
+        exit()
+    
+        
---
+++ steamtrades.py
@@ -0,0 +1,60 @@
+import requests
+import time
+from time import gmtime, strftime
+
+def enter_sleep_time():
+	global sleep_time
+	t = raw_input()
+
+	if t is '':
+		sleep_time = 2100
+		print('\nDefault time 35 min is set\n')
+
+	
+	else:
+		try:
+			t = int(t)
+			if t < 2100:
+				print '\nTime can not be less than 35 min\nEnter again\n'
+				enter_sleep_time()
+			else:
+				sleep_time = t
+		except ValueError, e:
+			print '\n Enter integer value\n'
+			enter_sleep_time()
+
+
+
+print '\nEnter the time for sleep in seconds (default 35 min)\n'
+print'\nEg 40 min then enter 2400\n'
+enter_sleep_time()
+
+
+
+no_of_bumps = 0
+link_data = [line.strip() for line in open('links_steam_trades.txt')]
+cookie_data = [line.strip() for line in open('cookie_steam_trades.txt')]
+cookies = {cookie_data[0]:cookie_data[1]}
+payload = {'form_key':'9ebde2e0527708a29d2889708d0a3bc7', 
+'do':'bump'}
+
+def bump():
+
+	for link in link_data:
+	      r = requests.post(link, data=payload, cookies=cookies)
+	      bumped_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+	      message = '\nBumped %s %s times\n at %s'%(link,no_of_bumps, bumped_time) 
+	      print message
+	      f = open('output.txt', 'a')
+	      
+	      f.write(message)
+	      f.close()
+	print '\n Total bumped %s time\n'%no_of_bumps
+
+while True:
+	print '\nBump Started.....\n'
+	no_of_bumps += 1
+	bump()
+	min_time = sleep_time/60
+	print '\nSleeping for %s min\n'%min_time
+	time.sleep(sleep_time)
